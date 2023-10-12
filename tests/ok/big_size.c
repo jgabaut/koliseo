633f10f0ddc5a4d3bcb31ef3994e500f1a3dfc86
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-10-12 04:20:23 +0200

    feat: add big_size test (#34)

---
+++ tests/ok/big_size.c
@@ -0,0 +1,12 @@
+#include "../../src/koliseo.h"
+
+int main(void) {
+    KOLISEO_DEBUG = 0; //Important. On crashes like this, as of 0.2.6, the KOLISEO_DEBUG_FP won't be closed properly.
+    Koliseo* k = kls_new(1024*1024*1024);
+
+    int* p = KLS_PUSH(k,int,1);
+
+    kls_free(k);
+    printf("That's a big Koliseo.\n");
+    return 0;
+}
