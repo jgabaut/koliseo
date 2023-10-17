8388769e5c7f9064e94e34e6b225041918e9e7fb
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-10-17 14:08:16 +0200

    chore: fix anvil tests

--- tests/error/bad_size.c
+++ tests/error/bad_size.c
@@ -1,7 +1,6 @@
 #include "../../src/koliseo.h"
 
 int main(void) {
-    KOLISEO_DEBUG = 0; //Important. On crashes like this, as of 0.2.6, the KOLISEO_DEBUG_FP won't be closed properly.
     Koliseo* k = kls_new(-1);
 
     int* p = KLS_PUSH(k,int,1);
