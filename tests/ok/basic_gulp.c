189cb455ff233e6d3ce4a4281881500d3dd700ce
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-11-09 00:45:05 +0100

    feat: add basic_gulp test (#45)

---
+++ tests/ok/basic_gulp.c
@@ -0,0 +1,19 @@
+// jgabaut @ github.com/jgabaut
+// SPDX-License-Identifier: GPL-3.0-only
+#include "../../src/koliseo.h"
+
+int main(void) {
+    const char* filepath = "./LICENSE";
+
+    Koliseo* k = kls_new(KLS_DEFAULT_SIZE*4);
+    char * gulped = NULL;
+    gulped = KLS_GULP_FILE(k, filepath);
+    if (gulped != NULL) {
+        printf("[Gulped file as C string]\n");
+        //printf("File: {\n\"%s\"\n}\n", gulped);
+    } else {
+        fprintf(stderr, "%s():  KLS_GULP_FILE() failed.\n",__func__);
+    }
+    kls_free(k);
+    return 0;
+}
