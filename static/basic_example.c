82e964295cfb07fa585b520352c89146aa48e0e5
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-04-08 20:48:18 +0200

    fix: basic_example.c using < 0.4.x macro signature

--- static/basic_example.c
+++ static/basic_example.c
@@ -14,7 +14,7 @@ int main(void)
     Koliseo *kls = kls_new_traced(KLS_DEFAULT_SIZE, "./static/debug_log.txt");
 
     //Use the arena (see demo for Koliseo_Temp usage)
-    Example *e = KLS_PUSH(kls, Example, 1);
+    Example *e = KLS_PUSH(kls, Example);
     e->val = 42;
 
     //Show contents to stdout
