e461b4a68829cf9d6a944578b4315904d09aa6cc
jgabaut <109908086+jgabaut@users.noreply.github.com>
2025-06-28 07:42:37 +0000

    chore: many_regions tests use kls_new_alloc_ext()

--- tests/error/many_temp_regions_typed.c
+++ tests/error/many_temp_regions_typed.c
@@ -2,7 +2,8 @@
 
 int main(void) {
 
-    Koliseo* kls = kls_new_alloc_ext(KLS_DEFAULT_SIZE, KLS_DEFAULT_ALLOCF, KLS_DEFAULT_FREEF, KLS_DEFAULT_HOOKS, KLS_DEFAULT_EXTENSION_DATA);
+    KLS_Err_Handlers err_handlers = KLS_DEFAULT_ERR_HANDLERS;
+    Koliseo* kls = kls_new_traced_alloc_handled_ext(KLS_DEFAULT_SIZE, "./static/debug_log.txt", KLS_DEFAULT_ALLOCF, KLS_DEFAULT_FREEF, err_handlers, KLS_DEFAULT_HOOKS, KLS_DEFAULT_EXTENSION_DATA);
 
     Koliseo_Temp* t_kls = kls_temp_start(kls);
     int iter=1;
