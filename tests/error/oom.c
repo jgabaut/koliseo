2ba3d199e1edfabc838e7482eaee7cb6690ea0de
jgabaut <109908086+jgabaut@users.noreply.github.com>
2025-08-01 13:05:44 +0000

    chore: add oom, oom_temp errortests

---
+++ tests/error/oom.c
@@ -0,0 +1,26 @@
+// jgabaut @ github.com/jgabaut
+// SPDX-License-Identifier: GPL-3.0-only
+
+typedef struct Example {
+    int val;
+} Example;
+
+#include "../../src/koliseo.h"
+
+int main(void)
+{
+    //Init the arena
+    Koliseo* kls = kls_new(500);
+
+    //Use the arena (see demo for Koliseo_Temp usage)
+    Example* e = NULL;
+    for(int i = 0; i < 500; i++) {
+        e = KLS_PUSH(kls,Example);
+    }
+    e->val = 42;
+
+    //Free the arena
+    kls_free(kls);
+    printf("Done test {\"%s\"}.\n",__FILE__);
+    return 0;
+}
