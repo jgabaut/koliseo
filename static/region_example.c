fbf337d4a98f0b82071fcf403e0d6a2612a1bbb7
jgabaut <109908086+jgabaut@users.noreply.github.com>
2025-12-28 23:49:25 +0000

    chore: fix static/region_example.c

--- static/region_example.c
+++ static/region_example.c
@@ -8,7 +8,7 @@ int main(void) {
 
     Koliseo* kls = kls_new(KLS_DEFAULT_SIZE);
 
-    printf("kls->extension_handlers.on_push_handler == {%p}\n", kls->hooks.on_push_handler);
+    printf("kls->extension_handlers.on_push_handler == {%p}\n", kls->hooks[KLS_AUTOREGION_EXT_SLOT].on_push_handler);
     printf("KLS_DEFAULT_EXTENSION_HANDLERS.on_push_handler == {%p}\n", KLS_DEFAULT_HOOKS.on_push_handler);
     int* foo = KLS_PUSH(kls, int);
 
@@ -17,7 +17,7 @@ int main(void) {
     int* bar = KLS_PUSH(kls, int);
     printf("foo: {%i}\n", *foo);
 
-    KLS_Autoregion_Extension_Data* data_pt = (KLS_Autoregion_Extension_Data*) kls->extension_data;
+    KLS_Autoregion_Extension_Data* data_pt = (KLS_Autoregion_Extension_Data*) kls->extension_data[KLS_AUTOREGION_EXT_SLOT];
 
     printf("Region list length: {%i}\n", kls_rl_length(data_pt->regs));
     kls_rl_showList(data_pt->regs);
